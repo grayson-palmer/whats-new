5b952564e556808a66962d4cb35e06feeea4c740
Grayson Palmer <gpalmer1385@gmail.com>
2020-01-29 19:05:16 -0700

    Create search form render

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SearchForm.css';
 
-// SEARCHFORM COMPONENT CODE GOES HERE
+class SearchForm extends Component {
+  constructor() {
+    super()
+    this.state = {} 
+  }
+
+  render() {
+    return (
+      <form>
+        <input className='search-input' type='text' placeholder='Search for news article here.' />
+        <button className='search-button' type='button'>Search Now</button>
+      </form>
+    )
+  }
+}
 
 export default SearchForm;
