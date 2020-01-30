20f4fed7bc58410df6e41a77f582f050f65786b9
Grayson Palmer <gpalmer1385@gmail.com>
2020-01-30 13:48:06 -0700

    Create handleChange test

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+describe('App', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  it('should set the state using the handleChange method to a different article type', () => {
+    const wrapper = shallow(<App />);
+    const mockPlaceholder = [{}];
+    const mockState = {articles: {
+      mockData: [{
+        id: 5,
+        headline: 'Its Thanos Time!',
+        img: 'imgPath',
+        description: 'Lets get purple',
+        url: 'urlPath'
+      }]
+    },
+    chosen: mockPlaceholder
+  }
+  wrapper.setState(mockState);
+
+  wrapper.instance().handleChange('mockData');
+  expect(wrapper.state('chosen')).toEqual(mockState.articles['mockData']);
+  })
+  
+})
